fa472698909480c22de9a69f8818520802630b97
deniskadrich <dev4long@live.com>
2021-10-04 17:25:08 -0400

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
     <Row title = "Action Movies" fetchUrl= {requests.fetchActionMovies}/>
     <Row title = "Comedy Movies" fetchUrl= {requests.fetchComedyMovies}/>
     <Row title = "Horror Movies" fetchUrl= {requests.fetchHorrorMovies}/>
-    <Row title = "Romance Movies" fetchUrl= {requests.fetchRomanceMovies}/>
+    <Row title = "Romantic Movies" fetchUrl= {requests.fetchRomanceMovies}/>
     <Row title = "Documentaries" fetchUrl= {requests.fetchDocumentaries}/>
     </div>
 
